3c10e5dfff29962c1996e4b0444724a59ced4483
Felipe Ros Pegini <felipe16699@gmail.com>
2023-12-03 23:50:11 -0300

    atualizando rota

--- my_app/lib/app/modules/atores/atores_module.dart
+++ my_app/lib/app/modules/atores/atores_module.dart
@@ -1,16 +1,23 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_app/app/modules/atores/atores_page.dart';
+import 'package:my_app/app/modules/atores/page/atores_detail_page.dart';
 
 class AtoresModule extends Module {
   @override
-  final List<Bind> binds = [
-  ];
+  final List<Bind> binds = [];
 
   @override
   final List<ModularRoute> routes = [
     ChildRoute(
       Modular.initialRoute,
       child: (_, args) => AtoresPage(),
+    ),
+    ChildRoute(
+      'atores/actor-details', 
+      child: (_, args) {
+        final actorDetails = args?.data['actorDetails'] ?? {}; 
+        return AtoresDetailsPage(actorDetails: actorDetails);
+      },
     ),
   ];
 }
